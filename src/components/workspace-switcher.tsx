8243ece1140016f8d2b2562af0f0d9e9dccfcafe
Binish <binishjoshi@pm.me>
2024-11-18 14:05:21 +0545

    feat: show current workspace and route in workspace switcher

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { RiAddCircleFill } from "react-icons/ri";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 import {
   Select,
@@ -14,7 +16,13 @@ import {
 } from "./ui/select";
 
 export function WorkspaceSwitcher() {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -22,7 +30,7 @@ export function WorkspaceSwitcher() {
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
