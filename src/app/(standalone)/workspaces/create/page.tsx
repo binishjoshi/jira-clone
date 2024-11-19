0c81ab1a66146f048104b54dcb4a591777c067b2
Binish <binishjoshi@pm.me>
2024-11-19 20:20:39 +0545

    fix: added route auth check

--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 
-export default function CreateWorkspacePage() {
+export default async function CreateWorkspacePage() {
+  const user = await getCurrent();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
     <div className="w-full lg:max-w-xl">
       <CreateWorkspaceForm />
