15650f11ba1abc33c3d185b4b4ca7207a2638587
Binish <binishjoshi@pm.me>
2024-12-06 20:18:28 +0545

    feat: data filter to filter tasks

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -1,18 +1,36 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc";
+import { TaskStatus } from "../types";
 
 interface UseGetTasksProps {
   workspaceId: string;
+  projectId?: string | null;
+  status?: TaskStatus | null;
+  assigneeId?: string | null;
+  dueDate?: string | null;
+  search?: string | null;
 }
 
-export const useGetTasks = ({ workspaceId }: UseGetTasksProps) => {
+export const useGetTasks = ({
+  workspaceId,
+  assigneeId,
+  dueDate,
+  projectId,
+  status,
+  search,
+}: UseGetTasksProps) => {
   const query = useQuery({
-    queryKey: ["tasks", workspaceId],
+    queryKey: ["tasks", workspaceId, projectId, status, assigneeId, dueDate],
     queryFn: async () => {
       const response = await client.api.tasks.$get({
         query: {
           workspaceId,
+          projectId: projectId ?? undefined,
+          status2: status ?? undefined,
+          assigneeId: assigneeId ?? undefined,
+          dueDate: dueDate ?? undefined,
+          search: search ?? undefined,
         },
       });
 
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,17 +1,42 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { useQueryState } from "nuqs";
+import { Loader, PlusIcon } from "lucide-react";
+
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useGetTasks } from "../api/use-get-tasks";
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { useTaskFilters } from "../hooks/use-task-filters";
 
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import DottedSeparator from "@/components/dotted-separator";
-import { useCreateTaskModal } from "../hooks/use-create-task-modal";
+import { DataFilters } from "@/features/tasks/components/data-filters";
 
 export function TaskViewSwitcher() {
+  const [{ status, assigneeId, dueDate, projectId }] = useTaskFilters();
+
+  const [view, setView] = useQueryState("task-view", {
+    defaultValue: "table",
+  });
+
+  const workspaceId = useWorkspaceId();
+
   const { open } = useCreateTaskModal();
+  const { data: tasks, isLoading: isLoadingTasks } = useGetTasks({
+    workspaceId,
+    projectId,
+    assigneeId,
+    status,
+    dueDate,
+  });
 
   return (
-    <Tabs className="flex-1 w-full border rounded-lg">
+    <Tabs
+      defaultValue={view}
+      onValueChange={setView}
+      className="flex-1 w-full border rounded-lg"
+    >
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
@@ -31,19 +56,25 @@ export function TaskViewSwitcher() {
           </Button>
         </div>
         <DottedSeparator className="my-4" />
-        Data filters
+        <DataFilters />
         <DottedSeparator className="my-4" />
-        <>
-          <TabsContent value="table" className="mt-0">
-            Data table
-          </TabsContent>
-          <TabsContent value="kanban" className="mt-0">
-            Data kanban
-          </TabsContent>
-          <TabsContent value="calendar" className="mt-0">
-            Data calendar
-          </TabsContent>
-        </>
+        {isLoadingTasks ? (
+          <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
+            <Loader className="size-5 animate-spin text-muted-foreground" />
+          </div>
+        ) : (
+          <>
+            <TabsContent value="table" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+            <TabsContent value="kanban" className="mt-0">
+              {JSON.stringify(tasks)}
+            </TabsContent>
+            <TabsContent value="calendar" className="mt-0">
+              Data calendar
+            </TabsContent>
+          </>
+        )}
       </div>
     </Tabs>
   );
